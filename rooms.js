f1b989000029e43c8caaf75c0cebe75a3ad0b3a8
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2023-09-11 16:25:30 +0600

    add to cart . 1 ta product akbar e add kora jabe

--- rooms.js
+++ rooms.js
@@ -56,6 +56,7 @@ range.addEventListener("input", () => {
 document.getElementById('sort-by-price-btn').addEventListener('click', () =>{
      allRooms.sort((a,b)=>{
         return parseFloat(a.price.$numberDecimal) < parseFloat(b.price.$numberDecimal)  ? 1: -1
+        // return parseFloat(a.price.$numberDecimal) > parseFloat(b.price.$numberDecimal)  ? 1: -1
     })
     // console.log(allRooms)
     displayRoomsData(allRooms)
